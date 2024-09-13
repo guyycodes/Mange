4c2e6c81a5a8aa0cdbe8a6e090c94f53ab4f5a6d
guymorganb <guymorganb@gmail.com>
2024-09-13 15:03:18 -0500

    added React.js client-mange, added springboot skeleton

---
+++ server/mange/src/main/java/com/mange/mange/MangeApplication.java
@@ -0,0 +1,13 @@
+package com.mange.mange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangeApplication.class, args);
+	}
+
+}
